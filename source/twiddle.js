aab349e5dd6b3ed9c591c324585b6bc7e5a5f5e7
Sebastian Felling <sebastian@felling.de>
2022-05-24 15:00:08 +0200

    fixed code issues

--- source/twiddle.js
+++ source/twiddle.js
@@ -1,7 +1,9 @@
+const version = "1.00";
+
 function createKeys(keyboardRowElement, chars)
 {
     chars.split("").map(char =>  {
-        let keyElement = document.createElement("span");
+        const keyElement = document.createElement("span");
         keyElement.innerHTML = char;
         keyboardRowElement.append(keyElement);
     });
@@ -9,13 +11,15 @@ function createKeys(keyboardRowElement, chars)
 function createRows(keyboardElement, rowsOfChars)
 {
     rowsOfChars.map(rowOfChars => {
-        let keyboardRowElement = document.createElement("div");
-        keyboardRowElement.classList.add('row');
+        const keyboardRowElement = document.createElement("div");
+        keyboardRowElement.classList.add("row");
         keyboardElement.append(keyboardRowElement);
         
         createKeys(keyboardRowElement, rowOfChars);
     });
 }
 
-let keyboardElement = document.getElementById("keyboard");
+const keyboardElement = document.getElementById("keyboard");
 createRows(keyboard, ["QWERTYUIOP", "ASDFGHJKL", "ZXCVBNM"]);
+
+console.debug(`Twiddle loaded v${version}.`);
